790720b33a2b938ae9fc2a897b541ed26a86f462
Matt Martz <matt@sivel.net>
2015-02-26 10:35:55 -0600

    Bump to 0.3.2

--- speedtest_cli.py
+++ speedtest_cli.py
@@ -15,7 +15,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 # Some global variables we use
 user_agent = 'speedtest-cli/%s' % __version__
