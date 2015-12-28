81182c1c941fe38109d51df498d6cdda7a203975
Matt Martz <matt@sivel.net>
2015-12-28 14:32:47 -0600

    Rename from speedtest_cli.py to speedtest.py, but maintain backwards compat with a symlink

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2012-2014 Matt Martz
+# Copyright 2012-2015 Matt Martz
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -57,7 +57,7 @@ except:
 
 setup(
     name='speedtest-cli',
-    version=find_version('speedtest_cli.py'),
+    version=find_version('speedtest.py'),
     description=('Command line interface for testing internet bandwidth using '
                  'speedtest.net'),
     long_description=long_description,
@@ -66,11 +66,11 @@ setup(
     author_email='matt@sivel.net',
     url='https://github.com/sivel/speedtest-cli',
     license='Apache License, Version 2.0',
-    py_modules=['speedtest_cli'],
+    py_modules=['speedtest'],
     entry_points={
         'console_scripts': [
-            'speedtest=speedtest_cli:main',
-            'speedtest-cli=speedtest_cli:main'
+            'speedtest=speedtest:main',
+            'speedtest-cli=speedtest:main'
         ]
     },
     classifiers=[
