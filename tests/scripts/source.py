a75ee8e097b3cb2318d7f81e6620fd922cd4d014
Matt Martz <matt@sivel.net>
2018-01-02 17:02:34 -0600

    foo

---
+++ tests/scripts/source.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+cmd = [sys.executable, 'speedtest.py', '--source', '127.0.0.1']
+
+p = subprocess.Popen(
+    cmd,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+
+stdout, stderr = p.communicate()
+
+if p.returncode != 1:
+    raise SystemExit('%s did not fail with exit code 1' % ' '.join(cmd))
+
+if b'Invalid argument' not in stderr:
+    raise SystemExit(
+        '"Invalid argument" not found in stderr:\n%s' % stderr.decode()
+    )
