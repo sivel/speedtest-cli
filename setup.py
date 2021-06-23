ff5a7ce7144fa0d228923a197932fb3c6944263d
Bastian Venthur <bastian.venthur@flixbus.com>
2021-06-23 11:00:48 +0200

    removed py310

--- setup.py
+++ setup.py
@@ -87,6 +87,5 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
     ]
 )
